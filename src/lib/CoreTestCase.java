db96a9d68cf7cc0eb19c4ede2ccaba1d55e1ff4c
Throcken90 <yu-yu@yandex.ru>
2021-11-01 18:32:58 +0300

    Refactoring FINISHED

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -3,6 +3,7 @@ package lib;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -24,11 +25,24 @@ public class CoreTestCase extends TestCase {
         capabilities.setCapability("appActivity", "org.wikipedia.main.MainActivity");
         capabilities.setCapability("app", "D:\\Java\\Lesson3\\Apks\\org.wikipedia.apk");
         driver = new AndroidDriver(new URL(AppiumUrl), capabilities);
+        this.rotateScreenPortrait();
     }
     @Override
     protected void tearDown() throws Exception
     {
         driver.quit();
         super.tearDown();
+    }
+    protected void rotateScreenPortrait()
+    {
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+    protected void rotateScreenLandscape()
+    {
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+    }
+    protected void backgroundApp(int seconds)
+    {
+        driver.runAppInBackground(seconds);
     }
 }
